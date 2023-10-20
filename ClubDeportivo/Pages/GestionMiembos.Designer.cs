dade824496ed7f4ac0f0a2925c811c5d04f5f96d
Isabella <isabellachiaradia@hotmail.com>
2023-10-19 21:57:57 -0300

    feat: Added Pages folder with GestionMiembros and PagosFacturacion cs in it

---
+++ ClubDeportivo/Pages/GestionMiembos.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace Dashboard_ClubDeportivo.pesañas
+{
+    partial class GestionMiembos
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GestionMiembos));
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            txbNombre = new TextBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            tbxApellido = new TextBox();
+            txbDni = new TextBox();
+            txbCorreo = new TextBox();
+            txbDireccion = new TextBox();
+            txbFechaNacimiento = new TextBox();
+            txbFechaInscripcion = new TextBox();
+            cbxEsSocio = new CheckBox();
+            dgtvListaSocios = new DataGridView();
+            btnAgregar = new CustomBotonDos();
+            LIMPIAR = new CustomBotonDos();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgtvListaSocios).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Microsoft Sans Serif", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(34, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(239, 26);
+            label1.TabIndex = 0;
+            label1.Text = "Gestión de miembros";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-1, 68);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(304, 43);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // txbNombre
+            // 
+            txbNombre.BackColor = Color.FromArgb(74, 102, 174);
+            txbNombre.BorderStyle = BorderStyle.None;
+            txbNombre.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txbNombre.ForeColor = Color.White;
+            txbNombre.Location = new Point(43, 80);
+            txbNombre.Margin = new Padding(3, 2, 3, 2);
+            txbNombre.Multiline = true;
+            txbNombre.Name = "txbNombre";
+            txbNombre.Size = new Size(214, 18);
+            txbNombre.TabIndex = 2;
+            txbNombre.Text = "Nombre";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(-1, 124);
+            pictureBox2.Margin = new Padding(3, 2, 3, 2);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(304, 43);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(-1, 182);
+            pictureBox3.Margin = new Padding(3, 2, 3, 2);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(304, 43);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 4;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(-1, 406);
+            pictureBox4.Margin = new Padding(3, 2, 3, 2);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(304, 43);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 5;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(-1, 350);
+            pictureBox5.Margin = new Padding(3, 2, 3, 2);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(304, 43);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 6;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(-1, 296);
+            pictureBox6.Margin = new Padding(3, 2, 3, 2);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(304, 43);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 7;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(-1, 241);
+            pictureBox7.Margin = new Padding(3, 2, 3, 2);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(304, 43);
+            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox7.TabIndex = 8;
+            pictureBox7.TabStop = false;
+            // 
+            // tbxApellido
+            // 
+            tbxApellido.BackColor = Color.FromArgb(74, 102, 174);
+            tbxApellido.BorderStyle = BorderStyle.None;
+            tbxApellido.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            tbxApellido.ForeColor = Color.White;
+            tbxApellido.Location = new Point(43, 136);
+            tbxApellido.Margin = new Padding(3, 2, 3, 2);
+            tbxApellido.Multiline = true;
+            tbxApellido.Name = "tbxApellido";
+            tbxApellido.Size = new Size(214, 18);
+            tbxApellido.TabIndex = 9;
+            tbxApellido.Text = "Apellido";
+            // 
+            // txbDni
+            // 
+            txbDni.BackColor = Color.FromArgb(74, 102, 174);
+            txbDni.BorderStyle = BorderStyle.None;
+            txbDni.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txbDni.ForeColor = Color.White;
+            txbDni.Location = new Point(43, 196);
+            txbDni.Margin = new Padding(3, 2, 3, 2);
+            txbDni.Multiline = true;
+            txbDni.Name = "txbDni";
+            txbDni.Size = new Size(214, 18);
+            txbDni.TabIndex = 10;
+            txbDni.Text = "DNI";
+            // 
+            // txbCorreo
+            // 
+            txbCorreo.BackColor = Color.FromArgb(74, 102, 174);
+            txbCorreo.BorderStyle = BorderStyle.None;
+            txbCorreo.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txbCorreo.ForeColor = Color.White;
+            txbCorreo.Location = new Point(43, 254);
+            txbCorreo.Margin = new Padding(3, 2, 3, 2);
+            txbCorreo.Multiline = true;
+            txbCorreo.Name = "txbCorreo";
+            txbCorreo.Size = new Size(214, 18);
+            txbCorreo.TabIndex = 11;
+            txbCorreo.Text = "Correo";
+            // 
+            // txbDireccion
+            // 
+            txbDireccion.BackColor = Color.FromArgb(74, 102, 174);
+            txbDireccion.BorderStyle = BorderStyle.None;
+            txbDireccion.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txbDireccion.ForeColor = Color.White;
+            txbDireccion.Location = new Point(43, 308);
+            txbDireccion.Margin = new Padding(3, 2, 3, 2);
+            txbDireccion.Multiline = true;
+            txbDireccion.Name = "txbDireccion";
+            txbDireccion.Size = new Size(214, 18);
+            txbDireccion.TabIndex = 12;
+            txbDireccion.Text = "Dirección";
+            // 
+            // txbFechaNacimiento
+            // 
+            txbFechaNacimiento.BackColor = Color.FromArgb(74, 102, 174);
+            txbFechaNacimiento.BorderStyle = BorderStyle.None;
+            txbFechaNacimiento.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txbFechaNacimiento.ForeColor = Color.White;
+            txbFechaNacimiento.Location = new Point(43, 362);
+            txbFechaNacimiento.Margin = new Padding(3, 2, 3, 2);
+            txbFechaNacimiento.Multiline = true;
+            txbFechaNacimiento.Name = "txbFechaNacimiento";
+            txbFechaNacimiento.Size = new Size(214, 18);
+            txbFechaNacimiento.TabIndex = 20;
+            txbFechaNacimiento.Text = "Fecha N.";
+            // 
+            // txbFechaInscripcion
+            // 
+            txbFechaInscripcion.BackColor = Color.FromArgb(74, 102, 174);
+            txbFechaInscripcion.BorderStyle = BorderStyle.None;
+            txbFechaInscripcion.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txbFechaInscripcion.ForeColor = Color.White;
+            txbFechaInscripcion.Location = new Point(43, 418);
+            txbFechaInscripcion.Margin = new Padding(3, 2, 3, 2);
+            txbFechaInscripcion.Multiline = true;
+            txbFechaInscripcion.Name = "txbFechaInscripcion";
+            txbFechaInscripcion.Size = new Size(214, 18);
+            txbFechaInscripcion.TabIndex = 21;
+            txbFechaInscripcion.Text = "Fecha I.";
+            // 
+            // cbxEsSocio
+            // 
+            cbxEsSocio.AutoSize = true;
+            cbxEsSocio.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            cbxEsSocio.ForeColor = Color.White;
+            cbxEsSocio.Location = new Point(43, 460);
+            cbxEsSocio.Margin = new Padding(3, 2, 3, 2);
+            cbxEsSocio.Name = "cbxEsSocio";
+            cbxEsSocio.Size = new Size(96, 24);
+            cbxEsSocio.TabIndex = 22;
+            cbxEsSocio.Text = "Es socio";
+            cbxEsSocio.UseVisualStyleBackColor = true;
+            // 
+            // dgtvListaSocios
+            // 
+            dgtvListaSocios.BackgroundColor = Color.White;
+            dgtvListaSocios.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgtvListaSocios.Location = new Point(315, 68);
+            dgtvListaSocios.Margin = new Padding(3, 2, 3, 2);
+            dgtvListaSocios.Name = "dgtvListaSocios";
+            dgtvListaSocios.RowHeadersWidth = 51;
+            dgtvListaSocios.RowTemplate.Height = 29;
+            dgtvListaSocios.Size = new Size(471, 310);
+            dgtvListaSocios.TabIndex = 23;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.BackColor = Color.FromArgb(96, 61, 140);
+            btnAgregar.Cursor = Cursors.Hand;
+            btnAgregar.FlatAppearance.BorderSize = 0;
+            btnAgregar.FlatStyle = FlatStyle.Flat;
+            btnAgregar.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnAgregar.ForeColor = Color.White;
+            btnAgregar.Location = new Point(318, 406);
+            btnAgregar.Margin = new Padding(3, 2, 3, 2);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(181, 39);
+            btnAgregar.TabIndex = 24;
+            btnAgregar.Text = "AGREGAR";
+            btnAgregar.UseVisualStyleBackColor = false;
+            // 
+            // LIMPIAR
+            // 
+            LIMPIAR.BackColor = Color.FromArgb(96, 61, 140);
+            LIMPIAR.Cursor = Cursors.Hand;
+            LIMPIAR.FlatAppearance.BorderSize = 0;
+            LIMPIAR.FlatStyle = FlatStyle.Flat;
+            LIMPIAR.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            LIMPIAR.ForeColor = Color.White;
+            LIMPIAR.Location = new Point(603, 406);
+            LIMPIAR.Margin = new Padding(3, 2, 3, 2);
+            LIMPIAR.Name = "LIMPIAR";
+            LIMPIAR.Size = new Size(181, 39);
+            LIMPIAR.TabIndex = 25;
+            LIMPIAR.Text = "LIMPIAR";
+            LIMPIAR.UseVisualStyleBackColor = false;
+            // 
+            // GestionMiembos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(74, 102, 174);
+            Controls.Add(LIMPIAR);
+            Controls.Add(btnAgregar);
+            Controls.Add(dgtvListaSocios);
+            Controls.Add(cbxEsSocio);
+            Controls.Add(txbFechaInscripcion);
+            Controls.Add(txbFechaNacimiento);
+            Controls.Add(txbDireccion);
+            Controls.Add(txbCorreo);
+            Controls.Add(txbDni);
+            Controls.Add(tbxApellido);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(txbNombre);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "GestionMiembos";
+            Size = new Size(789, 513);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgtvListaSocios).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private TextBox txbNombre;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private TextBox tbxApellido;
+        private TextBox txbDni;
+        private TextBox txbCorreo;
+        private TextBox txbDireccion;
+        private TextBox txbFechaNacimiento;
+        private TextBox txbFechaInscripcion;
+        private CheckBox cbxEsSocio;
+        private DataGridView dgtvListaSocios;
+        private CustomBotonDos btnAgregar;
+        private CustomBotonDos LIMPIAR;
+    }
+}
