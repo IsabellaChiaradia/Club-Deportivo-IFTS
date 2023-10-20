33e53aac3e6b897c6cf29e94b529ab919b2ca3ab
Hernan Santarelli <hernansantarelli87@gmail.com>
2023-10-20 17:19:07 -0300

    Se agregaron la entidad y clase de datos Miembro

---
+++ ClubDeportivo/Datos/Miembro.cs
@@ -0,0 +1,55 @@
+﻿using ClubDeportivo.Entidades;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubDeportivo.Datos
+{
+    public class Miembro
+    {
+        public string Nuevo_Miembro(E_Miembro miembro)
+        {
+            string? salida;
+            MySqlConnection sqlCon = new MySqlConnection();
+            try
+            {
+                sqlCon = Conexion.getInstancia().CrearConexion();
+                MySqlCommand comando = new MySqlCommand("NuevoMiembro",
+                sqlCon);
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.Add("Nom", MySqlDbType.VarChar).Value = miembro.Nombre;
+                comando.Parameters.Add("Ape", MySqlDbType.VarChar).Value = miembro.Apellido;
+                comando.Parameters.Add("Dni", MySqlDbType.VarChar).Value = miembro.DNI;
+                comando.Parameters.Add("EsSocio", MySqlDbType.Byte).Value = miembro.EsSocio ? 1 : 0;
+                comando.Parameters.Add("Correo", MySqlDbType.VarChar).Value = miembro.Correo;
+                comando.Parameters.Add("Direccion", MySqlDbType.VarChar).Value = miembro.Direccion;
+                comando.Parameters.Add("FNac", MySqlDbType.VarChar).Value = miembro.FechaNac;
+                comando.Parameters.Add("AlDia", MySqlDbType.Byte).Value = miembro.EstaAlDia ? 1 : 0;
+                comando.Parameters.Add("AptoM", MySqlDbType.Byte).Value = miembro.AptoMedico ? 1 : 0;
+                MySqlParameter ParCodigo = new MySqlParameter();
+                ParCodigo.ParameterName = "rta";
+                ParCodigo.MySqlDbType = MySqlDbType.Int32;
+                ParCodigo.Direction = ParameterDirection.Output;
+                comando.Parameters.Add(ParCodigo);
+                sqlCon.Open();
+                comando.ExecuteNonQuery();
+                salida = Convert.ToString(ParCodigo.Value);
+            }
+            catch (Exception ex)
+            {
+                salida = ex.Message;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                { sqlCon.Close(); };
+            }
+            return salida;
+
+        }
+    }
+}
---
+++ ClubDeportivo/Entidades/E_Miembro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubDeportivo.Entidades
+{
+    public class E_Miembro
+    {
+        public int IDMiembro { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? DNI { get; set; }
+        public Boolean EsSocio { get; set; }
+        public string? Correo { get; set; }
+        public string? Direccion { get; set; }
+        public string? FechaNac { get; set; }
+        public Boolean EstaAlDia { get; set; }
+        public Boolean AptoMedico { get; set; }
+        
+    }
+}
