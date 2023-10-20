901523cbf53680071b8313776c83925f3814ff41
Hernan Santarelli <hernansantarelli87@gmail.com>
2023-10-20 17:16:33 -0300

    Se creó carpeta de scripts

---
+++ Scripts/AltaMiembroSp.sql
@@ -0,0 +1,31 @@
+delimiter // 
+ drop procedure if exists NuevoMiembro//
+ create procedure NuevoMiembro(in Nom varchar(50),in Ape varchar(50), in Dni varchar(15), in EsSocio BOOLEAN, in Correo varchar(50), in Direccion varchar(50), in FNac varchar(50), in AlDia BOOLEAN, in AptoM BOOLEAN, out rta int)
+ begin
+     declare filas int default 0;
+	 declare existe int default 0;
+    
+     set filas = (select count(*) from miembro);
+     if filas = 0 then
+		set filas = 1001; /* consideramos a este numero como el primer numero de postulante */
+     else
+     /* -------------------------------------------------------------------------------
+		buscamos el ultimo numero de postulante almacenado para sumarle una unidad y
+		considerarla como PRIMARY KEY de la tabla
+   ___________________________________________________________________________ */
+		set filas = (select max(IDMiembro) + 1 from miembro);
+		
+		/* ---------------------------------------------------------
+			para saber si ya esta almacenado el postulante
+		------------------------------------------------------- */	
+		set existe = (select count(*) from miembro m where m.Correo = Correo and m.DNI = Dni);
+     end if;
+	 
+	  if existe = 0 then	 
+		 insert into miembro values(filas,Nom,Ape,Dni,EsSocio,Correo,Direccion,FNac,AlDia,AptoM);
+		 set rta  = filas; -- esto devuelve el id del postulante
+	  else
+		 set rta = existe; -- si devuelve 1 ya existe
+      end if;		 
+    
+     end //
---
+++ Scripts/ClubDeportivoDB.sql
@@ -0,0 +1,73 @@
+drop database if exists club_deportivo;
+create database club_deportivo;
+use club_deportivo; 
+
+create table roles(
+IDRol int,
+NomRol varchar(30),
+constraint primary key(IDRol)
+);
+
+insert into roles values
+(1,'Administrador'),
+(2,'Empleado');
+
+/*  modifique el auto_increment de miembro y tambien el tipo de dato en fecha de nacimiento  */
+create table Miembro(
+IDMiembro int,
+Nombre varchar(50),
+Apellido varchar(50),
+DNI varchar(15),
+EsSocio boolean,
+Correo varchar(50),
+Direccion varchar(50),
+FechaNac varchar(50),
+EstaAlDia boolean,
+AptoMedico boolean,
+constraint pk_miembro primary key (IDMiembro)
+);
+
+create table Usuario(
+IDUsuario int auto_increment,
+IDRol int,
+email varchar(50),
+contrasenia varchar(50),
+EstaActivo boolean,
+constraint pk_usuario primary key (IDUsuario),
+constraint fk_miembro_rol foreign key(IDRol) references roles (IDRol)
+);
+
+insert into usuario(IDUsuario,IDRol,email,contrasenia, EstaActivo) values
+(1, 1,'admin@admin',1234, 1),
+(2, 2,'noadmin@admin',1234, 0);
+
+create table Actividad(
+IDActiv int,
+Nombre varchar(20),
+Horario varchar(8),
+Fecha date,
+CupoMax int,
+Costo int,
+constraint pk_actividad primary key (IDActiv)
+);
+
+create table Inscripcion(
+IDInscrip int,
+IDMiembro int,
+IDActiv int,
+FechaInsc date,
+FechaVenc date,
+constraint pk_inscripcion primary key (IdInscrip),
+constraint fk_inscripcion_miembro foreign key (IDMiembro) references Miembro (IDMiembro),
+constraint fk_inscripcion_actividad foreign key (IDActiv) references Actividad (IDActiv)
+);
+
+create table Cuota(
+IDCuota int,
+Monto dec,
+FechaVenc date,
+EstaPagada boolean,
+IDActiv int,
+constraint pk_cuota primary key (IDCuota),
+constraint fk_cuota_actividad foreign key (IDActiv) references Actividad (IDActiv)
+);
